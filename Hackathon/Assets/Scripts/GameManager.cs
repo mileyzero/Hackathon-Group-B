6b9f07e2827ccfc00f3d7435595fcb5381a5c711
mileyzero <jerometan23000@hotmail.com>
2023-06-30 12:12:51 +0800

    patch 9.12.1

--- Hackathon/Assets/Scripts/GameManager.cs
+++ Hackathon/Assets/Scripts/GameManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public float happiness;
+    public float money;
+    public float popularity;
+
     // Start is called before the first frame update
     void Start()
     {
