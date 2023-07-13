2ad1e6705123c0a3a46cc50cd4496ce7ef7d8b93
OrianAkash <kaungthantwinsg@gmail.com>
2023-07-13 12:10:57 +0800

    truck and sea

--- Hackathon/Assets/Scripts/Car game/ConeSpawner.cs
+++ Hackathon/Assets/Scripts/Car game/ConeSpawner.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class ConeSpawner : MonoBehaviour
 {
     public GameObject cone;
+    public GameObject truck;
     public float maxPos;
     public float delaytimer = 1f;
+    public float randomnumber;
     float timer;
     // Start is called before the first frame update
     void Start()
@@ -17,12 +19,21 @@ public class ConeSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         timer -= Time.deltaTime;
-
+        
         if (timer <= 0) 
         {
+            int randomnumber = Random.Range(0, 7);
             Vector3 conePos = new Vector3(Random.Range(-maxPos, maxPos), transform.position.y, transform.position.z);
-            Instantiate(cone, conePos, transform.rotation);
+            if(randomnumber <=5)
+            {
+                Instantiate(cone, conePos, transform.rotation);
+            }
+            else
+            {
+                Instantiate(truck, conePos, transform.rotation);
+            }          
             timer = delaytimer;
         }    
     }
