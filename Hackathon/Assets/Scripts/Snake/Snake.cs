46c7a2575253ed8de28c8d81140a19651b723bcd
OrianAkash <kaungthantwinsg@gmail.com>
2023-07-17 10:58:24 +0800

    snake game

---
+++ Hackathon/Assets/Scripts/Snake/Snake.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2Int gridMoveDirection;
+    private Vector2Int gridposition;
+    private float gridMoveTimer;
+    private float gridMoveTimerMax;
+    private Level_Grid level_Grid;
+    
+    public void SetUp(Level_Grid level_Grid)
+    {
+        this.level_Grid = level_Grid;
+    }
+    private void Awake()
+    {
+       gridposition = new Vector2Int(10, 10);
+       gridMoveTimerMax = 0.5f;
+       gridMoveTimer = gridMoveTimerMax;
+       gridMoveDirection = new Vector2Int(1, 0);
+    }
+
+    private void Update()
+    {
+        HandleInput();
+        HandleGridMovement();
+
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (gridMoveDirection.y != -1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = +1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (gridMoveDirection.y != +1)
+            {
+                gridMoveDirection.x = 0;
+                gridMoveDirection.y = -1;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (gridMoveDirection.x != +1)
+            {
+                gridMoveDirection.x = -1;
+                gridMoveDirection.y = 0;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (gridMoveDirection.x != -1)
+            {
+                gridMoveDirection.x = +1;
+                gridMoveDirection.y = 0;
+            }
+        }
+    }
+
+    private void HandleGridMovement()
+    {
+        gridMoveTimer += Time.deltaTime;
+
+        if (gridMoveTimer >= gridMoveTimerMax)
+        {
+            gridposition += gridMoveDirection;
+            gridMoveTimer -= gridMoveTimerMax;
+
+            transform.position = new Vector3(gridposition.x, gridposition.y);
+            transform.eulerAngles = new Vector3(0, 0, GetAngleFromVector(gridMoveDirection)-90);
+
+            level_Grid.SnakeMoved(gridposition);
+        }
+    }
+
+    private float GetAngleFromVector(Vector2Int dir)
+    {
+        float n = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
+    }
+
+    public Vector2Int GetGridPosition()
+    {
+        return gridposition;
+    }
+}
