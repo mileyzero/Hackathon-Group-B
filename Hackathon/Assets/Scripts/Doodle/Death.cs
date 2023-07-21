c12648059a52b67c51f4b752ae4bf3ced2fca356
OrianAkash <kaungthantwinsg@gmail.com>
2023-07-21 14:16:47 +0800

    added platform auto delete

--- Hackathon/Assets/Scripts/Doodle/Death.cs
+++ Hackathon/Assets/Scripts/Doodle/Death.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
+
+    public GameObject losescreen;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "doodle")
         {
+            losescreen.SetActive(true);
             Debug.Log("Die");
+        }
+
+        if(collision.tag == "platform")
+        {
+            Destroy(collision.gameObject);
         }
     }
 }
