11877f3b2683da58d2fcf15f7d8712c1fe6563bd
OrianAkash <kaungthantwinsg@gmail.com>
2023-07-21 11:36:29 +0800

    doodle game

---
+++ Hackathon/Assets/Scripts/Doodle/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public Rigidbody2D rb;
+    public float moveX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = moveX;
+        rb.velocity = velocity;
+    }
+}
