e80269390d40b5e8f976f0bd2ec4bfb0baff51b7
OrianAkash <kaungthantwinsg@gmail.com>
2023-07-21 14:46:07 +0800

    lose doodle jump

--- Hackathon/Assets/Scripts/Doodle/Manager.cs
+++ Hackathon/Assets/Scripts/Doodle/Manager.cs
@@ -7,16 +7,25 @@ public class Manager : MonoBehaviour
     public GameObject platform;
 
     public int platformCount = 300;
+    public GameObject finalplatform;
     // Start is called before the first frame update
     void Start()
     {
         Vector3 spawnposition = new Vector3();
 
-        for (int i = 0; i < platformCount; i++)
+        for (int i = 0; i <= platformCount; i++)
         {
             spawnposition.y += Random.Range(2f, 3f);
             spawnposition.x = Random.Range(-5f, 5f);
-            Instantiate(platform, spawnposition, Quaternion.identity);
+            if(i == platformCount)
+            {
+                finalplatform = Instantiate(platform, spawnposition, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(platform, spawnposition, Quaternion.identity);
+            }
+
         }
     }
 
