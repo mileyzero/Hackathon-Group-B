1a70b9fcb9ce97b99a67b3b496b8ab7452a87201
OrianAkash <kaungthantwinsg@gmail.com>
2023-08-04 14:03:56 +0800

    added assets for golf

--- Hackathon/Assets/Scripts/Golf/Golf_Ball.cs
+++ Hackathon/Assets/Scripts/Golf/Golf_Ball.cs
@@ -8,9 +8,15 @@ using UnityEngine.Tilemaps;
 public class Golf_Ball : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject hole;
+
+    private bool isDragging;
+    private bool score;
+    public Vector2 ball_velocity;
+    private Animator animator;
+
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private TilemapCollider2D sand;
-    [SerializeField] private CircleCollider2D hole_colli;
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private float maxPower = 10f;
     [SerializeField] private float power = 2f;
@@ -18,15 +24,11 @@ public class Golf_Ball : MonoBehaviour
     [SerializeField] private float maxGoalSpeed = 4f;
     [SerializeField] private TextMeshProUGUI movesMade_txt;
     [SerializeField] private int movesMade = 0;
-    private GameObject hole;
 
-    private bool isDragging;
-    private bool score;
-    public Vector2 ball_velocity;
     void Start()
     {
         hole = GameObject.FindGameObjectWithTag("goal");
-        
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -40,11 +42,13 @@ public class Golf_Ball : MonoBehaviour
         }
         
     }
-
+    float angle;
     private void Player_Input() //Handles the player's input to move the ball
     {
         Vector2 input = Camera.main.ScreenToWorldPoint(Input.mousePosition); //mouse position in the game
-        float distance = Vector2.Distance(transform.position, input);
+        float distance = Vector2.Distance(transform.position, input); //calculate distance between ball and mouse
+        Vector2 direction = input - (Vector2)transform.position; //calcuate the direction from ball to mouse
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //calculating the angle of rotation to face towards the mouse
 
         if (Input.GetMouseButtonDown(0) && distance <= 0.5f) //when player drags from the ball
         {
@@ -73,6 +77,7 @@ public class Golf_Ball : MonoBehaviour
         Vector2 dir = (Vector2)transform.position - pos;
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, (Vector2)transform.position + Vector2.ClampMagnitude((dir * power) / 2, maxPower / 2));
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90f));
         float distance = Vector2.Distance((Vector2)transform.position, pos);
         if(distance > 4.5f)
         {
@@ -86,6 +91,8 @@ public class Golf_Ball : MonoBehaviour
     
     private void DragRelease(Vector2 pos) //calculates distance between mouse and ball then adds velocity to the ball in the opposite direction of the drag
     {
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle + 90f));
+        animator.SetTrigger("isHit");
         movesMade += 1;
         float distance = Vector2.Distance((Vector2)transform.position, pos);
         isDragging = false;
@@ -158,7 +165,7 @@ public class Golf_Ball : MonoBehaviour
     IEnumerator ballfly()
     {
         sand.enabled = false;
-        hole_colli.enabled = false;
+        hole.GetComponent<CircleCollider2D>().enabled = false;
         for (int i = 0; i<10; i++)
         {
             gameObject.transform.localScale = new Vector2(gameObject.transform.localScale.x + 0.05f, gameObject.transform.localScale.y + 0.05f);
@@ -172,7 +179,7 @@ public class Golf_Ball : MonoBehaviour
         }
 
         sand.enabled = true;
-        hole_colli.enabled = true;
+        hole.GetComponent<CircleCollider2D>().enabled = true;
         for (float i = 0.15f; i >= 0.1;i-=0.05f)
         {
             Debug.Log(i);
