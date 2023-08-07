74827200d19151cd1b82560dbed3cc95af3934b7
mileyzero <jerometan23000@hotmail.com>
2023-08-07 09:55:49 +0800

    7/8/2023 p1

--- Hackathon/Assets/Scripts/Snake/Snake_Menu.cs
+++ Hackathon/Assets/Scripts/Snake/Snake_Menu.cs
@@ -10,19 +10,23 @@ public class Snake_Menu : MonoBehaviour
     public Animator snekPlayAnim;
     public Animator snekQuitAnim;
 
+    //StartGame plays with coroutine
     public void StartGame()
     {
         StartCoroutine(PlayAnim(1f));
     }
 
+    //QuitGame plays with coroutine
     public void QuitGame()
     {
         StartCoroutine(QuitAnim(1f));
     }
 
+    //QuitAnim sets an animation trigger called 'Quit' which then gets WaitForSeconds with a float timer.
+    //After the timer has ended, it finds the main game by its tag and sets it to true and setting the snake game to false
+    //Which then loads the main game scene
     IEnumerator QuitAnim(float timer)
     {
-
         snekQuitAnim.SetTrigger("Quit");
 
         yield return new WaitForSeconds(timer);
@@ -33,6 +37,8 @@ public class Snake_Menu : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
+    //PlayAnim sets an animation trigger called 'Play' which then gets WaitForSeconds with a float timer.
+    //After the timer has ended, snakeGame sets active to true and the snake menu to false
     IEnumerator PlayAnim(float timer)
     {
         snekPlayAnim.SetTrigger("Play");
