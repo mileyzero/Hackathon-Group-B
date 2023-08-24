a5b86f3ab01a9c4d64f1a9c11a516eb3e0890884
OrianAkash <kaungthantwinsg@gmail.com>
2023-08-24 11:43:42 +0800

    snake guide

--- Hackathon/Assets/Scripts/Snake/GameHandler.cs
+++ Hackathon/Assets/Scripts/Snake/GameHandler.cs
@@ -7,13 +7,23 @@ public class GameHandler : MonoBehaviour
     [SerializeField] Snake snake;
     private Level_Grid levelgrid;
     public GameObject pauseMenu;
+
+    public GameObject guide;
     // Start is called before the first frame update
     void Start()
     {
         levelgrid = new Level_Grid(19,19);
-
+        guide.SetActive(true);
         snake.SetUp(levelgrid);
         levelgrid.SetUp(snake);
+
+        Time.timeScale = 0f;
+    }
+
+    public void Start_Click()
+    {
+        guide.SetActive(false);
+        Time.timeScale = 1f;
     }
     public void PauseMenu()
     {
